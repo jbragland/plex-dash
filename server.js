40115f4b6efcde8108daa1561a989da605593959
Julianne Ragland <jbragland@gmail.com>
2018-10-10 21:46:46 -0500

    Use body-parser

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 const app = express();
 
 const music = require('./routes/music');
 
+app.use(bodyparser.json());
 app.use('/', music);
 
 app.set('port', 3000);
