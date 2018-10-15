e633168852b70804cd2fee726639e33f2fae289b
Julianne Ragland <jbragland@gmail.com>
2018-10-14 19:47:46 -0500

    Log keys

--- routes/music.js
+++ routes/music.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.post('/', function(req, res, next) {
     console.log("Webhook received!");
-    console.log( JSON.parse(req) );
+    console.log( Object.keys(req) );
 });
 
 module.exports = router;
