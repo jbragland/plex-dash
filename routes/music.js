92121fcc6814a449bae623e07f46f78463b1075e
Julianne Ragland <jbragland@gmail.com>
2018-10-14 20:12:51 -0500

    Log payload

--- routes/music.js
+++ routes/music.js
@@ -6,7 +6,8 @@ var upload = multer({ dest: '/tmp/' });
 
 router.post('/', upload.single('thumb'), function(req, res, next) {
     console.log("Webhook received!");
-    console.log( 'req.body', req.body );
+    console.log( 'payload', req.body.payload );
+    console.log( 'THING?', req.body.payload.Metadata.title );
 });
 
 module.exports = router;
