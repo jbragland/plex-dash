92c61ac5e3781bc37dac17c2ac8c80774233b42d
Julianne Ragland <jbragland@gmail.com>
2018-10-14 19:51:44 -0500

    Log more stuf

--- routes/music.js
+++ routes/music.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 router.post('/', function(req, res, next) {
     console.log("Webhook received!");
-    console.log( Object.keys(req) );
+    console.log( 'req.params', req.params );
+    console.log( 'req.query', req.query );
+    console.log( 'req.res', req.res );
+    console.log( 'req.body keys', Object.keys(req.body) );
 });
 
 module.exports = router;
